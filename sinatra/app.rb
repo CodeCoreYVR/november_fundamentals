1adf40ab2db1b1b0ca77c84207f2b03251e27cf0
Tammam Kbeili <tammam.kbeili@gmail.com>
2014-11-22 11:33:45 -0800

    integrate data_mapper

--- sinatra/app.rb
+++ sinatra/app.rb
@@ -1,5 +1,22 @@
 require "sinatra"
 require "pony"
+require "data_mapper"
+
+DataMapper::setup(:default, 
+                  "sqlite3://#{Dir.pwd}/contact.db")
+
+# DataMapper will look for a table named contacts
+class Contact
+  include DataMapper::Resource
+
+  property :id, Serial
+  property :name, String
+  property :email, String
+  property :message, Text
+
+end
+
+Contact.auto_upgrade!
 
 # get -> HTTP verb
 # "/" -> URL
@@ -12,21 +29,25 @@ get "/contact" do
 end
 
 post "/contact" do
-  Pony.mail(to: "tam@codecore.ca",
-            from: params[:email],
-            reply_to: params[:email],
-            subject: "#{params[:name]} contacted you!",
-            body: params[:message],
-            via: :smtp,
-            via_options: {
-              address: "smtp.gmail.com",
-              port: "587",
-              user_name: "#{ENV[user_name]}",
-              password: "#{ENV[password]}",
-              authentication: :plain,
-              enable_starttls_auto: true
-            })
-  params.to_s
+  Contact.create(name: params[:name],
+                 email: params[:email],
+                 message: params[:message])
+  erb :thank_you, layout: :default
+  # Pony.mail(to: "tam@codecore.ca",
+  #           from: params[:email],
+  #           reply_to: params[:email],
+  #           subject: "#{params[:name]} contacted you!",
+  #           body: params[:message],
+  #           via: :smtp,
+  #           via_options: {
+  #             address: "smtp.gmail.com",
+  #             port: "587",
+  #             user_name: "#{ENV[user_name]}",
+  #             password: "#{ENV[password]}",
+  #             authentication: :plain,
+  #             enable_starttls_auto: true
+  #           })
+  # params.to_s
 end
 
 get "/hi" do
